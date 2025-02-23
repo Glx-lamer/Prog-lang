efe689c6d9fa910471714fdfe41ef1d5c496adbf
Jlx <66538395+Glx-lamer@users.noreply.github.com>
2025-02-23 21:52:49 +0300

    Added comparision with CPU, summ instead determinator

--- 3lab/GPU/main.cu
+++ 3lab/GPU/main.cu
@@ -8,30 +8,32 @@
 
 #define BLOCK_SIZE 16
 
-__global__ void SumRow(float* DevMatrix, int i, int diag, float k, int n) {
-	int j = diag + (blockIdx.x * blockDim.x + threadIdx.x);
+/* Kernel for transforming matrix */
+__global__ void SumRow(double* DevMatrix, int i, int diag, double k, int n) {
+    int j = diag + (blockIdx.x * blockDim.x + threadIdx.x);
 
-	if (j < n) {
-		DevMatrix[i * n + j] += k * DevMatrix[diag * n + j];
-	}
+    if (j < n) {
+        DevMatrix[i * n + j] = fma(k, DevMatrix[diag * n + j], DevMatrix[i * n + j]);
+    }
 }
 
-__global__ void CompK(float* DevMatrix, int diag, int n) {
-	dim3 threadsPerBlock(BLOCK_SIZE * BLOCK_SIZE);
-	dim3 blocksPerGrid((n + threadsPerBlock.x - 1) / threadsPerBlock.x);
-	int i = diag + 1 + (blockIdx.x * blockDim.x + threadIdx.x);
+/* Kernel for transforming matrix */
+__global__ void CompK(double* DevMatrix, int diag, int n) {
+    int i = diag + 1 + (blockIdx.x * blockDim.x + threadIdx.x);
 
-	if (i < n) {
-		float k = (-1.0) * DevMatrix[i * n + diag] / DevMatrix[diag * n + diag];
-		__syncthreads();
-		SumRow<<<blocksPerGrid, threadsPerBlock>>>(DevMatrix, i, diag, k, n);	
-	}
+    if (i < n) {
+        double k = (-1.0) * DevMatrix[i * n + diag] / DevMatrix[diag * n + diag];
+        dim3 threadsPerBlock(BLOCK_SIZE * BLOCK_SIZE);
+        dim3 blocksPerGrid((n + threadsPerBlock.x - 1) / threadsPerBlock.x);
+        SumRow<<<blocksPerGrid, threadsPerBlock>>>(DevMatrix, i, diag, k, n);
+    }
 }
 
-__global__ void CompDet(float* DevMatrix, int n, double* det) {
+/* Main kernel for computing determinator */
+__global__ void CompDet(double* DevMatrix, int n, double* det) {
     __shared__ double PartDet[BLOCK_SIZE];
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    int tid = threadIdx.x;
+    int idx = blockIdx.x * blockDim.x + threadIdx.x; /* Index like in 2-dimension list */
+    int tid = threadIdx.x; /* Thread index */
 
     PartDet[tid] = 1.0;
     if (idx < n) {
@@ -39,105 +41,162 @@ __global__ void CompDet(float* DevMatrix, int n, double* det) {
     }
 
     __syncthreads();
-	if (idx < n) {
-		for (int interval = blockDim.x / 2; interval > 0; interval /= 2) {
-			if (tid < interval) {
-				PartDet[tid] *= PartDet[tid + interval];
-			}
-			__syncthreads();
-		}
 
-		if (tid == 0) {
-			*det *= PartDet[0];
-		}
-	}
+    /* Reduction */
+    for (int interval = blockDim.x / 2; interval > 0; interval /= 2) {
+        if (tid < interval) {
+            PartDet[tid] += PartDet[tid + interval];
+        }
+        __syncthreads();
+    }
+
+    if (tid == 0) {
+        det[blockIdx.x] = PartDet[0];
+    }
 }
 
-void Gauss(float* DevMatrix, int n, double* det) {
-	for (int diag = 0; diag < n - 1; diag++) {
+/* Main computing func */
+void Gauss(double* DevMatrix, int n, double* det) {
+    for (int diag = 0; diag < n - 1; diag++) {
+        /* Computing needed quantity of blocks */
+        int i = n - (diag + 1);
+        dim3 threadsPerBlock(BLOCK_SIZE * BLOCK_SIZE);
+        dim3 blocksPerGrid((i + threadsPerBlock.x - 1) / threadsPerBlock.x);
 
-		int i = n - (diag + 1);
-		dim3 threadsPerBlock(BLOCK_SIZE * BLOCK_SIZE);
-		dim3 blocksPerGrid((i + threadsPerBlock.x - 1) / threadsPerBlock.x);
+        /* Transforming usual matrix to triangle kernel*/
+        CompK<<<blocksPerGrid, threadsPerBlock>>>(DevMatrix, diag, n);
+    }
 
-		CompK<<<blocksPerGrid, threadsPerBlock>>>(DevMatrix, diag, n); 
-	}
-
+    /* Computing needed quantity of blocks */
     dim3 threadsPerBlock(BLOCK_SIZE);
     dim3 blocksPerGrid((n + threadsPerBlock.x - 1) / threadsPerBlock.x);
-    CompDet<<<blocksPerGrid, threadsPerBlock>>>(DevMatrix, n, det);
+
+    double* DevPatDet;
+    cudaMalloc((void**)&DevPatDet, sizeof(double) * ((n + BLOCK_SIZE - 1) / BLOCK_SIZE));
+
+    /* Computing determinator kernel */
+    CompDet<<<blocksPerGrid, threadsPerBlock>>>(DevMatrix, n, DevPatDet);
+
+    /* Final reduction on device */
+    double* HstPatDet = (double*)malloc(sizeof(double) * ((n + BLOCK_SIZE - 1) / BLOCK_SIZE));
+    cudaMemcpy(HstPatDet, DevPatDet, sizeof(double) * ((n + BLOCK_SIZE - 1) / BLOCK_SIZE), cudaMemcpyDeviceToHost);
+
+    double ans = 1.0;
+    for (int i = 0; i < ((n + BLOCK_SIZE - 1) / BLOCK_SIZE); i++) {
+        ans += HstPatDet[i];
+    }
+
+    cudaMemcpy(det, &ans, sizeof(double), cudaMemcpyHostToDevice);
+
+    cudaFree(DevPatDet);
+    free(HstPatDet);
 }
 
-
-void PrintMatrix(float* HstMatrix, int n) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			printf("%8.2f", HstMatrix[i * n + j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
+void PrintMatrix(double* HstMatrix, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%8.2f", HstMatrix[i * n + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
-void FillMatrix(float* DevMatrix, int n) {
-	curandGenerator_t gen;
-
-	curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
-	curandSetPseudoRandomGeneratorSeed(gen, (unsigned long long)clock());
-	curandGenerateUniform(gen, DevMatrix, (n * n));
-
-	curandDestroyGenerator(gen);
+void FileToHstMatrix(double* HstMatrix, int n) {
+    FILE* file;
+    file = fopen("../test/cpu_matrix.txt", "r");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            fscanf(file, "%lf", &HstMatrix[i * n + j]);
+        }
+    }
+    fclose(file);
 }
 
+void Comparision(double HstDet) {
+    double Det;
+    FILE* file;
+    file = fopen("../test/cpu_out.txt", "r");
+    fscanf(file, "%lf", &Det);
+    fclose(file);
+    printf(">>> Expected determinant: %lf\n", Det);
+}
+
+void FillMatrix(double* DevMatrix, int n) {
+    curandGenerator_t gen;
+
+    curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
+    curandSetPseudoRandomGeneratorSeed(gen, (unsigned long long)clock());
+    curandGenerateUniformDouble(gen, DevMatrix, (n * n));
+
+    curandDestroyGenerator(gen);
+}
 
 int main(void) {
-	int n;
+    int n;
+    int mode;
 
-	printf("Enter size of square matrix >>>");
-	fflush(stdin);
-	fscanf(stdin, "%d", &n);
+    printf(">>> Mode:\n > 1 for usual <\n > 2 for test <\n>>> ");
+    fflush(stdin);
+    fscanf(stdin, "%d", &mode);
+    printf(">>> Matrix rank - ");
+    fflush(stdin);
+    fscanf(stdin, "%d", &n);
 
-	float* HstMatrix;
-	float* DevMatrix;
+    /* Host and device matrix */
+    double* HstMatrix;
+    double* DevMatrix;
 
-	HstMatrix = (float*)calloc(n * n, sizeof(float));
-	cudaMalloc((void**)&DevMatrix, sizeof(float) * n * n);
+    /* Init host matrix */
+    HstMatrix = (double*)calloc(n * n, sizeof(double));
+    if (mode == 2) {
+        FileToHstMatrix(HstMatrix, n);
+    }
 
-    FillMatrix(DevMatrix, n);
+    /* Init device matrix */
+    cudaMalloc((void**)&DevMatrix, sizeof(double) * n * n);
 
-	if (n <= 20) {
-		cudaMemcpy(HstMatrix, DevMatrix, sizeof(float) * n * n, cudaMemcpyDeviceToHost);
-		printf("Generated matrix:\n");
-		PrintMatrix(HstMatrix, n);
-	}
+    /* Fill device matrix from file with host matrix or randomly fill device matrix */
+    if (mode == 1) {
+        FillMatrix(DevMatrix, n);
+    }
+    else {
+        cudaMemcpy(DevMatrix, HstMatrix, sizeof(double) * n * n, cudaMemcpyHostToDevice);
+    }
 
+    if (n <= 10) {
+        cudaMemcpy(HstMatrix, DevMatrix, sizeof(double) * n * n, cudaMemcpyDeviceToHost);
+        printf(">>> Generated matrix:\n");
+        PrintMatrix(HstMatrix, n);
+    }
+
+    /* Host and device determinators */
     double HstDet = 1.0;
-    double *DevDet;
+    double* DevDet;
 
-	cudaMalloc((void**)&DevDet, sizeof(double));
+    /* Init device determinator */
+    cudaMalloc((void**)&DevDet, sizeof(double));
     cudaMemcpy(DevDet, &HstDet, sizeof(double), cudaMemcpyHostToDevice);
 
-	clock_t start = clock();
-	Gauss(DevMatrix, n, DevDet);
-	clock_t end = clock();
+    clock_t start = clock();
+    /* Main func */
+    Gauss(DevMatrix, n, DevDet);
+    clock_t end = clock();
 
     cudaMemcpy(&HstDet, DevDet, sizeof(double), cudaMemcpyDeviceToHost);
     cudaFree(DevDet);
 
-	cudaMemcpy(HstMatrix, DevMatrix, sizeof(float) * n * n, cudaMemcpyDeviceToHost);
-	
-	if (n <= 20) {
-        cudaMemcpy(HstMatrix, DevMatrix, sizeof(float) * n * n, cudaMemcpyDeviceToHost);
-		printf("Transformed matrix:\n");
-		PrintMatrix(HstMatrix, n);
-	}
+    cudaMemcpy(HstMatrix, DevMatrix, sizeof(double) * n * n, cudaMemcpyDeviceToHost);
 
-    printf("Determenator of matrix - %lf\n", HstDet);
-	float ms_duration = (float)(end - start) / CLOCKS_PER_SEC * 1000;
-	printf("Time to execute - %f ms\n", ms_duration);
+    if (mode == 2) {
+        Comparision(HstDet);
+    }
 
-	free(HstMatrix);
-	cudaFree(DevMatrix);
+    printf(">>> Matrix determinator: %lf\n", HstDet);
+    printf(">>> Time to calc - %f msec\n", (double)(end - start) / CLOCKS_PER_SEC * 1000);
 
-	return 0;
+    free(HstMatrix);
+    cudaFree(DevMatrix);
+
+    return 0;
 }
